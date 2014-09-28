158756f8ef8e433d96a49dddff5f006aaae8223d
pantuts <pantuts@gmail.com>
2014-09-28 19:28:12 +0800

    Update jquery.quicknote.js

--- jquery.quicknote.js
+++ jquery.quicknote.js
@@ -32,14 +32,24 @@
 		},
 		init: function() {
 			this.config = $.extend({}, this.defaults, this.options);
-			// detecting local storage
+			// DETECTING localStorage
 			if (Storage === void(0)) {
 				this.config.storage = false;
 			}
 			this.appendElem();
 			this.completeNote();
 		},
+		isURL: function(str) {
+			// CHECKING IF NOTE IS HAS URL FORMAT
+			if(/(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i.test(str)) {
+			    return true;
+			} else {
+			    return false;
+			}
+		},
 		appendElem: function() {
+			var isURL = this.isURL;
+
 			// THEME
 			if (this.config.theme == 'light') {
 				this.$el.addClass('qn_container_light').addClass('qn_container');
@@ -47,7 +57,7 @@
 				this.$el.addClass('qn_container');
 			} else {
 				console.log('Error: Theme >> ' + this.config.theme + ' not found.');
-				// set default
+				// SET DEFAULT
 				this.$el.addClass('qn_container');
 			}
 
@@ -66,39 +76,57 @@
 			$(divNotes).appendTo(this.$el);
 			$(notesInp).appendTo(this.$el.find('#notes'));
 
-			// check localStorage
+			// CHECK EXISTING NOTES IN localStorage
 			if (this.config.storage === true) {
 				var ls = JSON.parse(localStorage.getItem('quicknote')) || [];
 				if (ls) {
-					$.each(ls, function(i, obj) {
-						$('<span class="quicknote" id="' + ls[i].id + '"></span>').css({ display: 'table' }).stop().fadeIn('fast').appendTo('.qn_container #notes').text(ls[i].note);
-						$('<span class="close"></span>').prependTo('#' + ls[i].id);
+					// LOAD THE NOTES
+					$.each(ls, function(index, obj) {
+						$('<span class="quicknote" id="' + ls[index].id + '"></span>').css({ display: 'table' }).stop().fadeIn('fast').appendTo('.qn_container #notes').text(ls[index].note);
+						$('<span class="close"></span>').prependTo('#' + ls[index].id);
+						var qnText = ls[index].note;
+						if (isURL(qnText)) {
+							$('#' + ls[index].id).addClass('quicknote-bword');
+						}
 					});
 				}
 			}
 		},
 		completeNote: function() {
 			var storage = this.config.storage;
+			var isURL = this.isURL;
+
 			this.$el.on('keypress', '#notes input', function(e) {
 				// RETURN KEY PRESSED
 				if (e.which == 13 || e.keyCode == 13) {
 					var notesInpVal = $('#notes input').val();
+
 					if (notesInpVal) {
 						var uniqid = Date.now();
+
+						// CREATE NOTES
 						$('<span class="quicknote" id="qn_' + uniqid + '"></span>').css({ display: 'table' }).stop().fadeIn('fast').appendTo('.qn_container #notes').text(notesInpVal);
 						$('<span class="close"></span>').prependTo('#qn_' + uniqid);
+						// word-break: break-all IF HAS URL FORMAT
+						var qnText = $('#qn_' + uniqid).text();
+						if (isURL(qnText)) {
+							$('#qn_' + uniqid).addClass('quicknote-bword');
+						}
 						$('.qn_container #notes input').val('');
 
-						// save to localStorage
-						if (storage === true) {
-							var prevNotes = JSON.parse(localStorage.getItem('quicknote')) || [];
-							var newNote = {
-								'id': 'qn_' + uniqid,
-								'note': $('#qn_' + uniqid).text()
-							};
-							prevNotes.push(newNote);
-							localStorage.setItem('quicknote', JSON.stringify(prevNotes));
-						}
+						var id = 'qn_' + uniqid;
+                        var note = $('#qn_' + uniqid).text();
+                        var newNote = {
+                            'id': id,
+                            'note': note
+                        };
+
+                        // SAVE TO localStorage
+                        if (storage === true) {
+                            var prevNotes = JSON.parse(localStorage.getItem('quicknote')) || [];
+                            prevNotes.push(newNote);
+                            localStorage.setItem('quicknote', JSON.stringify(prevNotes));
+                        }
 					} else {
 						console.log('Empty note!');
 					}
@@ -114,21 +142,29 @@
 			this.$el.on('click', '#notes .close', function() {
 				$(this).each(function() {
 					$(this).parent('.quicknote').stop().fadeOut(100, function() {
-						var ID = $(this).attr('id');
-						// removal of item in localStorage
+						var id = $(this).attr('id');
+						var note = $(this).text();
+						var theNote = {
+                            'id': id,
+                            'note': note
+                        };
+
+						// REMOVAL OF ITEM IN localStorage
 						if (storage === true) {
 							var ls = JSON.parse(localStorage.getItem('quicknote')) || [];
 							if (ls) {
-								$.each(ls, function(i, obj) {
+								$.each(ls, function(index, obj) {
 									// console.log(ID);
-									if (obj.id == ID) {
-										ls.splice(i, 1);
+									if (obj.id == id) {
+										ls.splice(index, 1);
 										localStorage.setItem('quicknote', JSON.stringify(ls));
 										return false;
 									}
 								});
 							}
 						}
+
+						// REMOVE CURRENT ELEMENT FROM DOM
 						$(this).remove();
 					});
 				});
